e4b944e4b0ae063528c0d82afd84de6e6d44f9d7
Tibbe R <143705358+DVCTibbeR@users.noreply.github.com>
2024-01-31 09:33:32 +0100

    01.02

--- lightswitch.py
+++ lightswitch.py
@@ -1,9 +1,23 @@
 import tkinter as tk
+
 window = tk.Tk()
+window.config(bg="black")
 
-button = tk.Button(text="...", bg="white", fg="black")
+lightOn = False
+
+def toggleLightSwitch():
+	global lightOn
+
+	if lightOn:
+		button.config(text="Switch light off")
+		window.config(bg="black")
+	else:
+		button.config(text="Switch light on")
+		window.config(bg="yellow")
+
+	lightOn = not lightOn
+
+button = tk.Button(text="Switch light on", bg="white", fg="black", command=toggleLightSwitch)
 button.pack(pady = 20, padx = 20)
-
-
 
 window.mainloop()
