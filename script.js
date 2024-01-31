9975bd31695760fa7662497f5b8c6b683071e407
Tibbe R <143705358+DVCTibbeR@users.noreply.github.com>
2024-01-31 09:40:30 +0100

    02.02

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-var btn = document.createElement('button');
-btn.style.margin = '10px';
-btn.innerHTML = '...';
+let btn = document.createElement("button");
+
+btn.style.margin = "10px";
+btn.innerHTML = "Switch light on";
+
 document.body.appendChild(btn);
 
-// schijf hier tussen je code
+let lightOn = false
 
+btn.addEventListener("click", (_ev) => {
+	btn.textContent = lightOn ? "Switch light off" : "Switch light on";
+	document.body.style.backgroundColor = lightOn ? "black" : "yellow";
 
+	console.log(lightOn ? "light is off" : "light is on");
 
-// schijf hier tussen je code
+	lightOn = !lightOn;
+});
